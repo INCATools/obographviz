8c6a951761bf96a29e6b9294e1118c2cc6ded585
Patrick Kalita <pkalita@lbl.gov>
2022-05-03 09:50:23 -0700

    Add test case to `test equiv` suite

--- tests/equiv.test.js
+++ tests/equiv.test.js
@@ -6,13 +6,17 @@ var CliqueIndex = require('..').CliqueIndex;
 var exec = require('child_process').exec;
 
 describe('test equiv', function(){
-    var data = fs.readFileSync ('./tests/equivs.json');
-    og = JSON.parse(data);
-    var ogv = new OboGraphViz(og);
-    cx = new CliqueIndex();
-    console.log(og.equivalentNodesSets);
-    cliques = cx.findMaximalCliques(ogv._equivalentNodesSets);
-    console.log("CLIQUES="+cliques);
+
+    it('finds correct cliques', function () {
+        var data = fs.readFileSync ('./tests/equivs.json');
+        og = JSON.parse(data);
+        var ogv = new OboGraphViz(og);
+        cx = new CliqueIndex();
+        console.log(og.equivalentNodesSets);
+        cliques = cx.findMaximalCliques(ogv._equivalentNodesSets);
+        console.log("CLIQUES="+cliques);
+        assert.equal(cliques, 'W_3,X_3,Y_3,Z_3,W_1,X_1,Y_1,Z_1,W_2,X_2,Y_2,Z_2')
+    });
     
 });
 
